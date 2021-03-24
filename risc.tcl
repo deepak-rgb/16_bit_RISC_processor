cc8acd40462667f1daea7a25e7518ca5a287970d
deepak-rgb <51982127+deepak-rgb@users.noreply.github.com>
2021-03-24 22:37:15 +0530

    Add files via upload

---
+++ risc.tcl
@@ -0,0 +1,3 @@
+iverilog address_decoder.v ALU_16_bit.v control_unit.v data_mux.v data_mem.v gpr.v instruction_memory.v instruction_register.v page.v pipo.v program_counter.v reg_decoder.v reg_mux.v segment.v sel_mux.v sub_segment.v two_byte_cell.v risc.v tb_risc.v
+vvp a.out
+gtkwave risc.vcd
